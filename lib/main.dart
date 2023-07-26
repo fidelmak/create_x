8d12cf5e579230c6d7a075eab631dfad0546b9cc
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-07-26 02:14:16 +0100

    Please enter the commit message for your changes. Lines starting

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const CreateTask());
+}
+
+class CreateTask extends StatelessWidget {
+  const CreateTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
