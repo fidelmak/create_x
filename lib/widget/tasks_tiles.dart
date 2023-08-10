59359b0c89a3d719b1b680ba9e71f7a8bb0e34b7
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-08-10 04:52:00 +0100

    Changes to be committed:

--- lib/widget/tasks_tiles.dart
+++ lib/widget/tasks_tiles.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
-class TaskTiles extends StatelessWidget {
+class TaskTiles extends StatefulWidget {
+  @override
+  State<TaskTiles> createState() => _TaskTilesState();
+}
+
+class _TaskTilesState extends State<TaskTiles> {
+  bool isChecked = true;
+   checkBoxCallBack ( bool checkBoxState) {
+        setState(() {
+          isChecked = checkBoxState;
+        });
+      }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("This is a Task "),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (bool? value) {},
+      title: Text(
+        "This is a Task ",
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: CheckState(isChecked, checkBoxCallBack),
+    );
+  }
+}
+
+class CheckState extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+  CheckState(this.checkBoxState, this.toggleCheckBoxState);
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState;
     );
   }
 }
