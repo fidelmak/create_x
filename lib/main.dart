17749dcf75a246214aac83bf2cc33600d7f39a59
Oluborode Akintunde Paul <64453973+fidelmak@users.noreply.github.com>
2023-09-11 08:54:54 +0100

    with '#' will be ignored, and an empty message aborts the commit.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:create_x/model/tasksData.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/task_screen.dart';
+import 'task/my_card.dart';
 
 void main() {
   runApp(const CreateTask());
@@ -14,9 +15,8 @@ class CreateTask extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => TaskData(),
-      child: MaterialApp(
-        home: TaskScreen(),
-      ),
+      child: MaterialApp(home: const MyCard() //TaskScreen(),
+          ),
     );
   }
 }
